3414157c84bf277d39186effee353aa2ed7e1d6c
Nam Nguyen <nhnam.develop@gmail.com>
2016-06-05 15:11:08 +0800

    Release 0.0.5

--- Classes/UIKit+Shield.h
+++ Classes/UIKit+Shield.h
@@ -5,10 +5,6 @@
 //  Created by Nguyen Hoang Nam on 5/6/16.
 //  Copyright © 2016 Alan Nguyen. All rights reserved.
 //
-
-#import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-
 @interface UIView(Shield)
 @end
 
